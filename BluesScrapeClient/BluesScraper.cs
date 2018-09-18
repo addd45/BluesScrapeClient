058cd21cf7f84f8f68810ed9b73eb3ab630f38f8
Huss <addd45@gmail.com>
2018-09-17 22:13:53 -0500

    initial

---
+++ BluesScrapeClient/BluesScraper.cs
@@ -0,0 +1,102 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace BluesScrapeClient
+{
+    public enum GameStatuses { NotStarted, InAction, CriticalAction, Intermission, Final = 7}
+
+    class BluesScraper
+    {
+        string _url;
+        bool _bluesHome;
+        HttpClient _httpClient;
+        JToken _gameData, _liveData;
+
+        public BluesScraper(int gameCode)
+        {
+            _url = string.Format("https://statsapi.web.nhl.com/api/v1/game/{0}/feed/live", gameCode);
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Tuple<GameInfo, GameStatuses>> RefreshData()
+        {
+            try
+            {
+                //TODO: better error handlin
+                string rawJson = await _httpClient.GetStringAsync(_url);
+                if (string.IsNullOrWhiteSpace(rawJson))
+                {
+                    throw new Exception();
+                }
+
+                var jsonObj = JObject.Parse(rawJson);
+                _gameData = jsonObj["gameData"];
+                _liveData = jsonObj["liveData"];
+
+                var gameInfo = ExtractData();
+                string gameStatus = GetJsonValue(_gameData["status"]["statusCode"]);
+                bool fail = !Enum.TryParse(gameStatus, out GameStatuses status);
+
+                if (fail)
+                {
+                    //Failsafe
+                    status = GameStatuses.Final;
+                }
+
+                return Tuple.Create(gameInfo, status);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private GameInfo ExtractData()
+        {
+            GameInfo gameInfo = new GameInfo();
+
+            //Live Data
+            var lineScore = _liveData["linescore"];
+            gameInfo.BluesHome = (GetJsonValue(_gameData["teams"]["home"]["id"]) == "19");
+            gameInfo.HomeScore = int.Parse(GetJsonValue(lineScore["teams"]["home"]["goals"]));
+            gameInfo.AwayScore = int.Parse(GetJsonValue(lineScore["teams"]["away"]["goals"]));
+            gameInfo.HomeSOG = int.Parse(GetJsonValue(lineScore["teams"]["home"]["shotsOnGoal"]));
+            gameInfo.AwaySOG = int.Parse(GetJsonValue(lineScore["teams"]["away"]["shotsOnGoal"]));
+            gameInfo.Period = int.Parse(GetJsonValue(lineScore["currentPeriod"]));
+            long.TryParse(GetJsonValue(lineScore["currentPeriodTimeRemaining"]), out gameInfo.TimeRemaining);
+            gameInfo.TimeRemaining = (gameInfo.TimeRemaining == 0) ? -1 : gameInfo.TimeRemaining;
+
+            return gameInfo;
+        }
+
+        private string GetJsonValue(JToken token)
+        {
+            try
+            {
+                return token.Value<string>().Trim();
+            }
+            //not sure if needed but why not
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public bool BluesHome { get => _bluesHome; set => _bluesHome = value; }
+
+    }
+
+    struct GameInfo
+    {
+        public bool BluesHome;
+        public int HomeScore;
+        public int HomeSOG;
+        public int AwayScore;
+        public int AwaySOG;
+        public long TimeRemaining;
+        public int Period;
+    }
+}
---
+++ BluesScrapeClient/Program.cs
@@ -0,0 +1,143 @@
+﻿using MQTTnet;
+using MQTTnet.Adapter;
+using MQTTnet.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BluesScrapeClient
+{
+    class Program
+    {
+        static BluesScraper _scraper;
+        static IMqttClient _mqttClient;
+        static readonly string _mqttTopic = "Other/BluesScore";
+
+        static async Task Main(string[] args)
+        {
+            var mqttInit = InitializeMqtt("192.168.0.111", "homeassistant", "sb4517");
+
+            int temp = 2017020245;
+            _scraper = new BluesScraper(temp);
+
+            _mqttClient = await mqttInit;
+            await Execute();
+        }
+
+        static async Task<IMqttClient> InitializeMqtt(string addr, string user, string pw)
+        {
+            var factory = new MqttFactory();
+            var ret = factory.CreateMqttClient();
+            ret.Disconnected += Mqtt_Disconnected;
+
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId("BluesScraper")
+                .WithTcpServer(addr)
+                .WithCredentials(user, pw)
+                //.WithCleanSession()
+                .Build();
+
+            try
+            {
+                await ret.ConnectAsync(options);
+            }
+            catch(MqttConnectingFailedException e)
+            {
+                switch (e.ReturnCode)
+                {
+                        //idk
+                    case MQTTnet.Protocol.MqttConnectReturnCode.ConnectionAccepted:
+                        break;
+                        //protocol issue
+                    case MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedUnacceptableProtocolVersion:
+                        break;
+                        //idk
+                    case MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedIdentifierRejected:
+                        break;
+                        //server problem
+                    case MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedServerUnavailable:
+                        break;
+                        //auth problem
+                    case MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword:
+                    case MQTTnet.Protocol.MqttConnectReturnCode.ConnectionRefusedNotAuthorized:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return ret;
+        }
+
+        static async Task Execute()
+        {
+            while (true)
+            {
+                var data = await _scraper.RefreshData();
+                
+                TimeSpan delay = (GetDelayTime(data.Item2));
+                await SendData(data.Item1);
+
+                //End execution 
+                if (delay == default)
+                {
+                    break;
+                }
+
+                Thread.Sleep(delay);
+            }
+        }
+
+        private static TimeSpan GetDelayTime(GameStatuses status)
+        {
+            switch (status)
+            {
+                case GameStatuses.CriticalAction:
+                    return TimeSpan.FromSeconds(1);
+                case GameStatuses.Intermission:
+                case GameStatuses.NotStarted:
+                    return TimeSpan.FromSeconds(60);
+                case GameStatuses.InAction:
+                    return TimeSpan.FromSeconds(5);
+                case GameStatuses.Final:
+                default:
+                    return default;
+            }
+
+        }
+
+        static async Task SendData(GameInfo gameInfo)
+        {
+            string json = JsonConvert.SerializeObject(gameInfo);
+
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(_mqttTopic)
+                .WithAtMostOnceQoS()
+                .WithPayload(json)
+                .Build();
+            try
+            {
+                await _mqttClient.PublishAsync(message);
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+
+        private static async void Mqtt_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            //Never was connected in the first place
+            if (!e.ClientWasConnected)
+            {
+                throw e.Exception;
+            }
+            else
+            {
+                //TODO retry logic
+            }
+        }
+
+    }
+}
